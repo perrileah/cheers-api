29d63b82d553a3e1151182f393962cf2a776fc1a
Leah Perri <perrileah2023@gmail.com>
2023-07-04 14:36:15 -0500

    sessions crud fixed but not showing in browser inspection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,15 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-    render template: "users/new"
-  end
-
   def create
-    user = User.new(
+    user = User.create(
       name: params[:name],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
     )
     if user.save
-      session[:user_id] = @user.id
-      redirect_to "/"
+      render json: { message: "User created successfully" }, status: :created
     else
-      render :new, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
 end
