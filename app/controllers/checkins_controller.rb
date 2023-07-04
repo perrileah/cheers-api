88049d3ea1e5d5a22837122584708f30b531bcd1
Leah Perri <perrileah2023@gmail.com>
2023-07-04 13:46:23 -0500

    adds checkin update and destroy actions and adds model validations

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -12,12 +12,36 @@ class CheckinsController < ApplicationController
       image: params[:image],
       comments: params[:comments],
     )
-    
+
     render :show
   end
 
   def show
     @checkin = Checkin.find_by(id: params[:id])
     render :show
+  end
+
+  def update
+    @checkin = Checkin.find_by(id: params[:id])
+    @checkin.update(
+      brewery_id: params[:brewery_id] || @checkin.brewery_id,
+      user_id: params[:user_id] || @checkin.user_id,
+      rating: params[:rating] || @checkin.rating,
+      image: params[:image] || @checkin.image,
+      comments: params[:comments] || @checkin.comments,
+    )
+    # if @checkin.valid? #happy path
+    @checkin.save
+    render :show
+    # else #sad path
+    #     render :json {errors: @checkin.errors.full_messages}
+    # end
+
+  end
+
+  def destroy
+    @checkin = Checkin.find_by(id: params[:id])
+    @checkin.destroy
+    render json: { message: "Destroyed!" }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  has_many :checkins
+  has_many :breweries, through: :checkins
 end
