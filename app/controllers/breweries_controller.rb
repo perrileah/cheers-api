9de2b9fb0061fd373bd6712b9411d4956d72b942
Leah Perri <perrileah2023@gmail.com>
2023-07-06 08:40:12 -0500

    fixes crud actions for checkins and tries to fix login and logout actions

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,4 +4,9 @@ class BreweriesController < ApplicationController
     data = JSON.parse(response.body)
     render json: data
   end
+
+  # def show
+  #   @checkin = Checkin.find_by(id: params[:id])
+  #   render :show
+  # end
 end
--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -3,4 +3,39 @@ class CheckinsController < ApplicationController
     @checkins = Checkin.all
     render json: @checkins
   end
+
+  def create
+    if current_user
+    @checkin = Checkin.create(
+      brewery_id: params[:brewery_id],
+      user_id: params[:user_id],
+      rating: params[:rating],
+      image_url: params[:image_url],
+      comments: params[:comments],
+    )
+    render :show
+  end
+
+  def show
+    @checkin = Checkin.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @checkin = Checkin.find_by(id: params[:id])
+    @checkin.update(
+      brewery_id: params[:brewery_id],
+      user_id: params[:user_id],
+      rating: params[:rating],
+      image_url: params[:image_url],
+      comments: params[:comments],
+    )
+    render :show
+  end
+
+  def destroy
+    @checkin = Checkin.find_by(id: params[:id])
+    @checkin.destroy
+    render json: { message: "Checkin destroyed successfully" }
+  end
 end
