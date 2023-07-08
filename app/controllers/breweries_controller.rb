cf647173f1ddaf45aeab816bbbcf0522f8fa1bd1
Leah Perri <perrileah2023@gmail.com>
2023-07-08 12:04:03 -0500

    all crud actions work for checkins and breweries except update action

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -18,4 +18,31 @@ class BreweriesController < ApplicationController
     )
     render :show
   end
+
+  def show
+    @brewery = Brewery.find_by(id: params[:id])
+    render :show
+  end
+
+  def update
+    @brewery = Brewery.find_by(id: params[:id])
+    @brewery.update(
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip],
+      tap_list: params[:tap_list],
+      longitude: params[:longitude],
+      latitude: params[:latitude],
+      website_url: params[:website_url],
+    )
+    render :show
+  end
+
+  def destroy
+    @brewery = Brewery.find_by(id: params[:id])
+    @brewery.destroy
+    render json: { message: "Brewery destroyed successfully" }
+  end
 end
